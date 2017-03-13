a0948ceb70b9f7e736f3b976accfadcd900725be
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-13 00:51:14 -0400

    Extract API into class

--- lib/tablescript/api.rb
+++ lib/tablescript/api.rb
@@ -17,51 +17,73 @@
 
 $all_tables = {}
 
-def table(name, &blk)
-  begin
-    new_table = Tablescript::Table.new(name, Tablescript::DiceRoller.new)
-    new_table.build(&blk)
-    $all_tables[name] = new_table
-  rescue StandardError => e
-    puts e
-    exit
+module Tablescript
+  ##
+  # Api
+  #
+  class Api
+    def self.table(name, &blk)
+      begin
+        new_table = Table.new(name, DiceRoller.new)
+        new_table.build(&blk)
+        $all_tables[name] = new_table
+      rescue StandardError => e
+        puts e
+        exit
+      end
+    end
+
+    def self.roll_on(name)
+      ensure_table_exists(name)
+      $all_tables[name].roll
+    end
+
+    def self.roll_on_and_ignore_duplicates(name, times, *args)
+      ensure_table_exists(name)
+      $all_tables[name].roll_and_ignore_duplicates(times, args)
+    end
+
+    def self.lookup(name, roll)
+      ensure_table_exists(name)
+      $all_tables[name].lookup(roll)
+    end
+
+    def self.roll_dice(dice)
+      Tablescript::DiceRoller.new.roll(dice.dup)
+    end
+
+    def self.choose(options)
+      options[DiceRoller.new.random_value_in_range(1..options.size) - 1]
+    end
+
+    private
+
+    def self.ensure_table_exists(name)
+      raise "No table named '#{name}'" if $all_tables[name].nil?
+    end
   end
+end
+
+def table(name, &blk)
+  Tablescript::Api.table(name, &blk)
 end
 
 def roll_on(name)
-  raise "No table named '#{name}'" if $all_tables[name].nil?
-  $all_tables[name].roll
+  Tablescript::Api.roll_on(name)
 end
 
 def roll_on_and_ignore_duplicates(name, times, *args)
-  begin
-    raise "No table named '#{name}'" if $all_tables[name].nil?
-    $all_tables[name].roll_and_ignore_duplicates(times, args)
-  rescue StandardError => e
-    puts e
-    exit
-  end
+  Tablescript::Api.roll_on_and_ignore_duplicates(name, times, *args)
 end
 
 def lookup(name, roll)
-  begin
-    raise "No table named '#{name}'" if $all_tables[name].nil?
-    $all_tables[name].lookup(roll)
-  rescue StandardError => e
-    puts e
-    exit
-  end
+  Tablescript::Api.lookup(name, roll)
 end
 
 def roll_dice(dice)
-  begin
-    Tablescript::DiceRoller.new.roll(dice.dup)
-  rescue StandardError => e
-    puts e
-    exit
-  end
+  Tablescript::Api.roll_dice(dice)
 end
 
 def choose(options)
-  options[Tablescript::DiceRoller.new.random_value_in_range(1..options.size) - 1]
+  Tablescript::Api.choose(options)
 end
