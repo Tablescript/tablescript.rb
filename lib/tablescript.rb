e19e840d68c9e2c2da5ce29b70de542ae487ef4c
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-13 00:02:47 -0400

    Fix project names in license preamble

--- lib/tablescript.rb
+++ lib/tablescript.rb
@@ -1,6 +1,6 @@
 # Copyright 2015 Jamie Hale
 #
-# This file is part of the Tablescript.rb gem.
+# This file is part of the Tablescript gem.
 #
 # Tablescript.rb is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with Tablescript.  If not, see <http://www.gnu.org/licenses/>.
     
 require 'tablescript/dice_roller'
 require 'tablescript/roll_descriptor'
