0b1edc43e2605614ad1af94e3e3d1d39a547781f
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-13 02:09:28 -0400

    Add RollSet

---
+++ lib/tablescript/roll_set.rb
@@ -0,0 +1,28 @@
+
+module Tablescript
+  ##
+  # RollSet
+  #
+  class RollSet
+    attr_reader :rolls
+
+    def initialize(*args)
+      @rolls = Set.new
+      args.map(&:add)
+    end
+
+    def include?(roll)
+      @rolls.include?(roll)
+    end
+
+    def add(r)
+      if r.is_a?(Integer)
+        @rolls.add(r)
+      elsif r.is_a?(Range)
+        r.to_a.map { |e| @rolls.add(e) }
+      elsif roll.is_a?(RollSet)
+        @rolls.merge(r.rolls)
+      end
+    end
+  end
+end
