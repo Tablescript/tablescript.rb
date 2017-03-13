0a6aa4c7180f6e3bd188cdb4730a81fb5975acd5
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-13 18:42:59 -0400

    Add library spec

---
+++ spec/tablescript/library_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+module Tablescript
+  describe Library do
+    it 'is a singleton' do
+      expect { Library.instance }.not_to raise_error
+    end
+
+    describe 'add' do
+      let(:table) { double('table', name: :table) }
+      let(:library) { Library.clone.instance }
+
+      it 'knows it has added table' do
+        library.add(table)
+        expect(library.table?(:table)).to be true
+      end
+
+      it 'retrieves the table' do
+        library.add(table)
+        expect(library.table(:table)).to eq(table)
+      end
+
+      describe 'duplicates' do
+        it 'throws when trying to add a duplicate' do
+          library.add(table)
+          expect { library.add(table) }.to raise_error(Exception)
+        end
+      end
+    end
+  end
+end
