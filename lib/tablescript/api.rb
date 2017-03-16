7ccbe40810e725cee162d8407fed33c7dc102e4b
Jamie Hale <jamie@smallarmyofnerds.com>
2017-03-16 17:50:22 -0400

    Refresh copyright headers

--- lib/tablescript/api.rb
+++ lib/tablescript/api.rb
@@ -1,13 +1,13 @@
-# Copyright 2015 Jamie Hale
+# Copyright 2017 Jamie Hale
 #
 # This file is part of the Tablescript gem.
 #
-# Tablescript.rb is free software: you can redistribute it and/or modify
+# Tablescript is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Tablescript.rb is distributed in the hope that it will be useful,
+# Tablescript is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
